6a8a8137a3bed407de79a2b79ee47121404fadef
hieubd <buiduchieuuet16@gmail.com>
2023-10-25 22:01:38 +0700

    noti

--- App.tsx
+++ App.tsx
@@ -5,14 +5,31 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
+  Alert,
   SafeAreaView,
   View,
 } from 'react-native';
 import WebViewComponent from './components/WebViewComponent';
+import messaging from '@react-native-firebase/messaging';
+import { getToken, notificationListen, requestUserPermission } from './utils';
 
 function App(): JSX.Element {
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    requestUserPermission();
+    notificationListen();
+    getToken();
+  }, [])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+import messaging from '@react-native-firebase/messaging';
+
 export function makeid(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -8,4 +10,42 @@ export function makeid(length) {
         counter += 1;
     }
     return result;
+}
+
+export async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+        console.log('Authorization status:', authStatus);
+    }
+}
+
+export const notificationListen = () => {
+    messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+            'Notification caused app to open from background state:',
+            remoteMessage.notification,
+        );
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+            if (remoteMessage) {
+                console.log(
+                    'Notification caused app to open from quit state:',
+                    remoteMessage.notification,
+                );
+            }
+        });
+}
+
+export const getToken = async () => {
+    await messaging().registerDeviceForRemoteMessages();
+    const token = await messaging().getToken();
+    console.log(token);
 }
